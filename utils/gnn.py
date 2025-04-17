cad3c4d8d3ed7a0dec838c8d6aae09eaf3f0d2c6
ags3927 <aniruddhags3927@gmail.com>
2025-04-17 02:17:35 +0000

    :bug: removed old code to train gnn with all graphs
    Bug-1:
    GNN training expected older version of dataset and did not use any torch dataloader.
    
    Fix:
    Replaced the code to accommodate separate train and test dataloaders.
    
    Bug-2:
    This stems from the earlier bug. The code expected to process all graphs for all samples in a single step.
    This would lead to OOMs if the cohort is sufficiently large.
    
    Fix:
    Replaced gnn training and testing code to process the graph from a single slide in one step.
    Losses are computed for each such step.

--- utils/gnn.py
+++ utils/gnn.py
@@ -20,7 +20,12 @@ def gnn_train(gnn, dataloader, optimizer, alpha=0):
     
     for batch in dataloader:
         # Get the batch data
-        slide_indices, edge_indices, labels, patch_embeddings = batch
+        slide_index, edge_indices, labels, patch_embeddings = batch
+        
+        # Squeeze to remove the extra dimension
+        labels = labels.squeeze()
+        edge_indices = edge_indices.squeeze()
+        patch_embeddings = patch_embeddings.squeeze()
     
         # Track history if only in train
         with torch.set_grad_enabled(True):
@@ -30,14 +35,14 @@ def gnn_train(gnn, dataloader, optimizer, alpha=0):
         output = output.view_as(labels)
 
         # Calculate the MSE loss
-        mse = F.mse_loss(output, train_lbls)
+        mse = F.mse_loss(output, labels)
     
         # Calculate the correlation
         output = output.T
-        train_lbls = train_lbls.T
+        labels = labels.T
         corr = []
-        for g in range(train_lbls.shape[0]):
-            corr.append(pearsonr(output[g].cpu().detach(), train_lbls[g].cpu().detach())[0])
+        for g in range(labels.shape[0]):
+            corr.append(pearsonr(output[g].cpu().detach(), labels[g].cpu().detach())[0])
         corr = torch.tensor(corr)
     
         # Average the correlation using torch
@@ -63,64 +68,81 @@ def gnn_train(gnn, dataloader, optimizer, alpha=0):
     
     return train_mse, train_corr
 
-def gnn_test_save(plot_path, gnn, input, edge_index, test_lbls, data, config):
+def gnn_test_save(plot_path, gnn, dataloader, data, config):
     # Set the model to evaluation mode
     gnn.eval()
     
     # Get the number of genes
     num_genes = data['num_genes']
     
-    with torch.set_grad_enabled(False):
-        output = gnn(input, edge_index)
+    # Create variables to store the test losses and correlations
+    test_mse, test_mae, test_corr = [], [], []
+    
+    for batch in dataloader:
+        # Get the batch data
+        slide_index, edge_indices, labels, patch_embeddings = batch
         
-    # Remove the last / and the last word from config['output_dir'] to get the base directory
-    base_dir = '/'.join(config['output_dir'].split('/')[:-1])       
+        # Squeeze to remove the extra dimension
+        labels = labels.squeeze()
+        edge_indices = edge_indices.squeeze()
+        patch_embeddings = patch_embeddings.squeeze()
+    
+        with torch.set_grad_enabled(False):
+            output = gnn(patch_embeddings, edge_indices)
+            
+        # Remove the last / and the last word from config['output_dir'] to get the base directory
+        base_dir = '/'.join(config['output_dir'].split('/')[:-1])       
 
-    # Create the directory to save the predictions
-    preds_save_path = f'{base_dir}/preds/'
-    Path(preds_save_path).mkdir(parents=True, exist_ok=True)
-    
-    
-    # Separate the output counts into the val slice dimensions save the results
-    start = 0
-    # Iterate over the slides and save the predictions
-    for idx, slide in enumerate(data['slides']):
-        # Skip the slides that are in the train set
-        if data['train_slides'][idx]:
-            continue
+        # Create the directory to save the predictions
+        preds_save_path = f'{base_dir}/preds/'
+        Path(preds_save_path).mkdir(parents=True, exist_ok=True)
         
-        # Create a numpy array of zeros with the shape of (num_spots, num_genes)
-        predicted_counts = np.zeros(shape=(data['spotnum'][idx], num_genes))
-        non_zero = data['nonzero'][idx]
-        non_zero_count = np.sum(non_zero)
+        # Separate the output counts into the val slice dimensions save the results
+        # Iterate over the slides and save the predictions
+        for idx, slide in enumerate(data['slides']):
+            # Skip the slides that are in the train set
+            if data['train_slides'][idx]:
+                continue
+            
+            # Create a numpy array of zeros with the shape of (num_spots, num_genes)
+            predicted_counts = np.zeros(shape=(data['spotnum'][idx], num_genes))
+            
+            # Assign the predicted counts to the non-zero indices. We have to do this because when we train the model, we only use the non-zero spots. So the predicted counts are only for the non-zero spots. While saving we assign those predicted counts to the non-zero spots in appropriate indices.
+            predicted_counts[data['nonzero'][idx]] = output.cpu().detach().numpy()
+            
+            # Save the predicted counts
+            np.save(f'{preds_save_path}/{slide}.npy', predicted_counts)
         
-        # Assign the predicted counts to the non-zero indices. We have to do this because when we train the model, we only use the non-zero spots. So the predicted counts are only for the non-zero spots. While saving we assign those predicted counts to the non-zero spots in appropriate indices.
-        predicted_counts[non_zero] = output.cpu().detach().numpy()[start:start+non_zero_count]
-        start += non_zero_count
+        output = output.type(labels.dtype)
+        output = output.view_as(labels)
         
-        # Save the predicted counts
-        np.save(f'{preds_save_path}/{slide}.npy', predicted_counts)
+        # Calculate the MSE and MAE loss
+        mse_loss = F.mse_loss(output, labels)
+        mae_loss = F.l1_loss(output, labels)
+        
+        # Calculate the correlation
+        output = output.T
+        labels = labels.T
+        corr = []
+        for g in range(num_genes):
+            corr.append(pearsonr(output[g].cpu().detach(), labels[g].cpu().detach())[0])
+        corr = torch.tensor(corr)
+        
+        # Add the loss and correlation to the testing losses and correlations
+        test_mse.append(mse_loss.item())
+        test_mae.append(mae_loss.item())
+        test_corr.append(corr)
     
-    output = output.type(test_lbls.dtype)
-    output = output.view_as(test_lbls)
-    
-    # Calculate the MSE and MAE loss
-    mse_loss = F.mse_loss(output, test_lbls)
-    mae_loss = F.l1_loss(output, test_lbls)
-    
-    # Calculate the correlation
-    output = output.T
-    test_lbls = test_lbls.T
-    corr = []
-    for g in range(num_genes):
-        corr.append(pearsonr(output[g].cpu().detach(), test_lbls[g].cpu().detach())[0])
-    corr = torch.tensor(corr)
+    # Average the testing losses and correlations
+    test_mse = np.mean(test_mse)
+    test_mae = np.mean(test_mae)
+    test_corr = np.mean(test_corr)
     
     # Save the results to a json file
     results = {
-        'mse': mse_loss.item(),
-        'mae': mae_loss.item(),
-        'corr': np.average(corr.cpu().numpy())
+        'mse': test_mse,
+        'mae': test_mae,
+        'corr': test_corr
     }
     results_file_path = f'{plot_path}/results.json'
     with open(results_file_path, 'w') as f:
@@ -128,59 +150,57 @@ def gnn_test_save(plot_path, gnn, input, edge_index, test_lbls, data, config):
            
     return
 
-def gnn_test(gnn, input, edge_index, test_lbls, num_genes):
+def gnn_test(gnn, dataloader, num_genes):
     # Set the model to evaluation mode
     gnn.eval()
     
-    # Track history if only in train
-    with torch.set_grad_enabled(False):
-        output = gnn(input, edge_index)
+    # Create variables to store the test losses and correlations
+    test_mse, test_mae, test_corr = [], [], []
+    
+    for batch in dataloader:
+        # Get the batch data
+        slide_index, edge_indices, labels, patch_embeddings = batch
+        
+        # Squeeze to remove the extra dimension
+        labels = labels.squeeze()
+        edge_indices = edge_indices.squeeze()
+        patch_embeddings = patch_embeddings.squeeze()
+    
+        # Track history if only in train
+        with torch.set_grad_enabled(False):
+            output = gnn(patch_embeddings, edge_indices)
 
-    output = output.type(test_lbls.dtype)
-    output = output.view_as(test_lbls)
+        output = output.type(labels.dtype)
+        output = output.view_as(labels)
+        
+        # Calculate the MSE and MAE loss
+        mse = F.mse_loss(output, labels)
+        mae = F.l1_loss(output, labels)
     
-    # Calculate the MSE and MAE loss
-    mse = F.mse_loss(output, test_lbls)
-    mae = F.l1_loss(output, test_lbls)
+        # Calculate the correlation
+        output = output.T
+        labels = labels.T
+        corr = []
+        for g in range(num_genes):
+            corr.append(pearsonr(output[g].cpu().detach(), labels[g].cpu().detach())[0])
+        corr = torch.tensor(corr)
     
-    # Calculate the correlation
-    output = output.T
-    test_lbls = test_lbls.T
-    corr = []
-    for g in range(num_genes):
-        corr.append(pearsonr(output[g].cpu().detach(), test_lbls[g].cpu().detach())[0])
-    corr = torch.tensor(corr)
-    
-    # Average the correlation
-    corr = np.average(corr.cpu().numpy())
-    
-    return mse, mae, 
+        # Average the correlation
+        corr = np.average(corr.cpu().numpy())
+        
+        # Add the loss and correlation to the testing losses and correlations
+        test_mse.append(mse.item())
+        test_mae.append(mae.item())
+        test_corr.append(corr)
 
-def gnn_block(data, image_datasets, config, graph_datasets):
-    x = {}
-    
-    # Get the number of non-zero spots for the train and val sets
-    train_nonzero, val_nonzero = [], []
-    train_features, val_features = [], []
-    
-    for i in range(len(data['slides'])):
-        if data['train_slides'][i]:
-            train_nonzero.append(data['nonzero'][i])
-            train_features.append(data['patch_embeddings'][i])
-        elif data['val_slides'][i]:
-            val_nonzero.append(data['nonzero'][i])
-            val_features.append(data['patch_embeddings'][i])
-    
-    # Concatenate the patch embeddings and non-zero indices
-    train_features = np.concatenate(train_features)
-    val_features = np.concatenate(val_features)
-    train_nonzero = np.concatenate(train_nonzero)
-    val_nonzero = np.concatenate(val_nonzero)
-    
-    # Patch embeddings concatenated
-    x["train"] = torch.tensor(train_features[train_nonzero]).to(config['device'])
-    x["val"] = torch.tensor(val_features[val_nonzero]).to(config['device'])
-    
+    # Average the testing losses and correlations
+    test_mse = np.mean(test_mse)
+    test_mae = np.mean(test_mae)
+    test_corr = np.mean(test_corr)
+        
+    return test_mse, test_mae, test_corr
+
+def gnn_block(data, dataloaders, config):    
     if config['mode'] in ['gnn_test']:
         # Create the GNN model
         if config['GNN']['type'] == 'GAT':
@@ -193,7 +213,7 @@ def gnn_block(data, image_datasets, config, graph_datasets):
         # Test the model
         # Get the directory of the model path
         plot_path = os.path.dirname(model_path)
-        gnn_test_save(plot_path, gnn, x['val'], graph_datasets['val'][0], torch.tensor(image_datasets['val'].y).type(torch.double).to(config['device']), data, config)
+        gnn_test_save(plot_path, gnn, dataloaders['val'], data, config)
         
         del gnn
     elif config['mode'] == 'gnn_train':
@@ -211,13 +231,13 @@ def gnn_block(data, image_datasets, config, graph_datasets):
 
             # Train the model
             for i in range(config['GNN']['epochs'] + 1):
-                train_mse, train_corr = gnn_train(gnn, x['train'], graph_datasets['train'][0], torch.tensor(image_datasets['train'].y).to(config['device']), optimizer)
+                train_mse, train_corr = gnn_train(gnn, dataloaders['train'], optimizer)
                 
                 # Run validation every 40 epochs and step the scheduler
                 if(i % 40 == 0):
                     scheduler.step()
                     
-                    test_mse, test_mae, test_corr = gnn_test(gnn, x['val'], graph_datasets['val'][0], torch.tensor(image_datasets['val'].y).type(torch.double).to(config['device']), data['num_genes'])
+                    test_mse, test_mae, test_corr = gnn_test(gnn, dataloaders['val'], data['num_genes'])
                     
                     # Print the training and validation losses and correlations
                     print(f'Epoch: {i}, Train MSE: {train_mse}, Train Corr: {train_corr}')
@@ -227,7 +247,7 @@ def gnn_block(data, image_datasets, config, graph_datasets):
             plot_path = config['output_dir'] + "/" + str(run_idx) + "/gnn/"
             Path(plot_path).mkdir(parents=True, exist_ok=True)
             
-            gnn_test_save(plot_path, gnn, x['val'], graph_datasets['val'][0], torch.tensor(image_datasets['val'].y).type(torch.double).to(config['device']), data, config)
+            gnn_test_save(plot_path, gnn, dataloaders['val'], data, config)
             
             model_path = plot_path + f"model_state_dict.pt"
             torch.save(gnn.cpu().state_dict(), model_path)
